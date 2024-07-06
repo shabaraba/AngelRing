b010a52fbd2429e37158b0552089ae6b85a4af4e
shabaraba <work.shabaraba@gmail.com>
2024-07-07 01:41:01 +0900

    feat: use mysql and show files saved in mysql

--- go-app/main.go
+++ go-app/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
     "bytes"
+    "database/sql"
+    "encoding/json"
     "fmt"
     "image"
     "image/jpeg"
+    _ "image/png"
     "io"
     "log"
     "net/http"
@@ -14,10 +17,58 @@ import (
 
     "github.com/gin-gonic/gin"
     "github.com/nfnt/resize"
+    _ "github.com/go-sql-driver/mysql"
 )
 
+var db *sql.DB
+
+func init() {
+    var err error
+    // MySQLの接続情報を適切に設定してください
+    db, err = sql.Open("mysql", os.Getenv("DB_CONNECTION"))
+    if err != nil {
+        log.Fatal(err)
+    }
+}
+
+type FileRecord struct {
+    ID   int    `json:"id"`
+    Title string `json:"title"`
+    Path string `json:"path"`
+    ThumbnailPath string `json:"thumbnailPath"`
+    CreatedAt string `json:"createdAt"`
+    UpdatedAt string `json:"updatedAt"`
+}
+
+func getImages(c *gin.Context) {
+    // MySQLにデータを保存
+    files, err := db.Query("SELECT * FROM files")
+    if err != nil {
+        c.String(http.StatusInternalServerError, "database insert err: %s", err.Error())
+        return
+    }
+    // defer rows.Close()
+
+    var results []FileRecord
+    for files.Next() {
+        var r FileRecord
+        if err := files.Scan(&r.ID, &r.Title, &r.Path, &r.ThumbnailPath, &r.CreatedAt, &r.UpdatedAt); err != nil {
+            log.Fatal(err)
+        }
+        results = append(results, r)
+    }
+    // 結果をJSONに変換
+    jsonData, err := json.Marshal(results)
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+        return
+    }
+
+    c.JSON(http.StatusOK, gin.H{ "data": string(jsonData) })
+}
+
 func uploadImage(c *gin.Context) {
-    err := c.Request.ParseMultipartForm(10 << 20) // 10 MBのメモリを使用
+    err := c.Request.ParseMultipartForm(10 << 20)
     if err != nil {
         c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to parse form"})
         return
@@ -38,15 +89,14 @@ func uploadImage(c *gin.Context) {
         }
         defer src.Close()
 
-        // 画像をデコード
         img, _, err := image.Decode(src)
         if err != nil {
+            fmt.Printf("image decode err: %s", err.Error())
             c.String(http.StatusInternalServerError, "image decode err: %s", err.Error())
             return
         }
 
         thumbnail := resize.Resize(100, 100, img, resize.Lanczos3)
-        // サムネイルをJPEGとしてエンコード
         var buf bytes.Buffer
         if err := jpeg.Encode(&buf, thumbnail, nil); err != nil {
             c.String(http.StatusInternalServerError, "thumbnail encode err: %s", err.Error())
@@ -54,14 +104,14 @@ func uploadImage(c *gin.Context) {
         }
 
         fileName := fmt.Sprintf("%d_%s", time.Now().UnixNano(), file.Filename)
+        savePath := filepath.Join("static/images", fileName)
         saveThumbnailPath := filepath.Join("static/thumbnails", fileName)
 
-        if err := c.SaveUploadedFile(file, filepath.Join("static/images", file.Filename)); err != nil {
+        if err := c.SaveUploadedFile(file, savePath); err != nil {
             c.String(http.StatusInternalServerError, "save file err: %s", err.Error())
             return
         }
 
-        // サムネイルを保存
         out, err := os.Create(saveThumbnailPath)
         if err != nil {
             c.String(http.StatusInternalServerError, "create thumbnail file err: %s", err.Error())
@@ -74,6 +124,15 @@ func uploadImage(c *gin.Context) {
             c.String(http.StatusInternalServerError, "save thumbnail err: %s", err.Error())
             return
         }
+
+
+        // MySQLにデータを保存
+        _, err = db.Exec("INSERT INTO files (title, path, thumbnail_path) VALUES (?, ?, ?)",
+            file.Filename, savePath, saveThumbnailPath)
+        if err != nil {
+            c.String(http.StatusInternalServerError, "database insert err: %s", err.Error())
+            return
+        }
     }
 
     c.JSON(http.StatusOK, gin.H{"message": "Upload complete"})
@@ -81,11 +140,8 @@ func uploadImage(c *gin.Context) {
 
 func main() {
     router := gin.Default()
-
-    // 静的ファイルの提供
     router.Static("/static", "./static")
-
-    // 画像アップロードエンドポイント
+    router.GET("/api/images", getImages)
     router.POST("/api/images", uploadImage)
 
     if err := router.Run(":8080"); err != nil {
--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -4,18 +4,41 @@ import Modal from 'react-modal';
 
 Modal.setAppElement('#root');
 
+type Pathes = {
+  path: string; 
+  thumbnailPath: string;
+}
+
 const App: React.FC = () => {
-  const [files, setFiles] = useState<File[]>([]);
+  const [uploadingFiles, setUploadingFiles] = useState<File[]>([]);
+  const [filePathes, setFilePathes] = useState<Array<Pathes>>([]);
   const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
   const [uploading, setUploading] = useState<boolean>(false);
 
   useEffect(() => {
-    setFiles([])
-  }, []);
+    fetch('/api/images')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(response => {
+        const data = JSON.parse(response.data);
+        console.log(data);
+        setFilePathes(data.map((d: Pathes) => {return {"path": d.path, "thumbnailPath": d.thumbnailPath}}));
+      })
+      .catch(error => {
+        console.log(error);
+        // setError(error.message);
+        // setLoading(false);
+      });
+    setUploadingFiles([])
+  }, [uploading]);
 
-  const openModal = (image: File) => {
-    setSelectedImage(URL.createObjectURL(image));
+  const openModal = (path: string) => {
+    setSelectedImage(path);
     setModalIsOpen(true);
   };
 
@@ -27,7 +50,7 @@ const App: React.FC = () => {
     setUploading(true);
     try {
       const formData = new FormData();
-      files.forEach((file) => {
+      uploadingFiles.forEach((file) => {
         formData.append('files', file);
       });
       const response = await fetch('/api/images', {
@@ -38,7 +61,7 @@ const App: React.FC = () => {
       });
       if (response.ok) {
         console.log('Upload successful');
-        setFiles([]);
+        setUploadingFiles([]);
       } else {
         console.error('Upload failed');
       }
@@ -51,7 +74,7 @@ const App: React.FC = () => {
 
   return (
     <div>
-      <Dropzone onDrop={(acceptedFiles) => setFiles([...files, ...acceptedFiles])}>
+      <Dropzone onDrop={(acceptedFiles) => setUploadingFiles([...uploadingFiles, ...acceptedFiles])}>
         {({ getRootProps, getInputProps }) => (
           <section>
             <div {...getRootProps()} style={{ border: '1px solid black', padding: '20px', textAlign: 'center', cursor: 'pointer' }}>
@@ -62,19 +85,30 @@ const App: React.FC = () => {
         )}
       </Dropzone>
       <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-        {files.map((file, index) => (
+        {uploadingFiles.map((file, index) => (
           <img
             key={index}
             src={URL.createObjectURL(file)}
             alt={`Uploaded file ${index}`}
-            onClick={() => openModal(file)}
+            onClick={() => openModal(URL.createObjectURL(file))}
             style={{ width: '100px', height: '100px', margin: '10px', cursor: 'pointer' }}
           />
         ))}
       </div>
-      <button onClick={handleUpload} disabled={files.length === 0 || uploading}>
+      <button onClick={handleUpload} disabled={uploadingFiles.length === 0 || uploading}>
         {uploading ? 'Uploading...' : 'Upload'}
       </button>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {filePathes.map((filePath, index) => (
+          <img
+            key={index}
+            src={filePath.thumbnailPath}
+            alt={`Uploaded file ${index}`}
+            onClick={() => openModal(filePath.path)}
+            style={{ width: '100px', height: '100px', margin: '10px', cursor: 'pointer' }}
+          />
+        ))}
+      </div>
       <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
         {selectedImage && <img src={selectedImage} alt="Full size" style={{ maxWidth: '100%' }} />}
       </Modal>
